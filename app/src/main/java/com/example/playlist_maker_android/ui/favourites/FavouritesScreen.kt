0dce890c6e15d331ff26e3c200d9cf631b53da2c
Vlad <aliknegrov2007@mail.ru>
2026-02-03 23:35:44 +0300

    fix(UI): change FavouritesScreen UI on UI in figma

--- app/src/main/java/com/example/playlist_maker_android/ui/favourites/FavouritesScreen.kt
+++ app/src/main/java/com/example/playlist_maker_android/ui/favourites/FavouritesScreen.kt
@@ -1,5 +1,6 @@
 package com.example.playlist_maker_android.ui.favourites
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -9,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
@@ -19,7 +21,11 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import com.example.playlist_maker_android.R
 import com.example.playlist_maker_android.ui.search.components.ArrowBackButton
 import com.example.playlist_maker_android.ui.theme.Dimensions
 import com.example.playlist_maker_android.ui.viewmodel.PlaylistsViewModel
@@ -52,21 +58,58 @@ fun FavouritesScreen(
                         .padding(start = 4.dp, top = 4.dp, bottom = 4.dp)
                 ) {
                     ArrowBackButton(onBack)
+                    Text(
+                        text = stringResource(R.string.favourites_panel_header_text),
+                        modifier = Modifier
+                            .padding(start = 12.dp),
+                        style = MaterialTheme.typography.titleLarge.copy(
+                            color = MaterialTheme.colorScheme.onPrimary,
+                            textAlign = TextAlign.Center
+                        ),
+                    )
                 }
             }
 
-            Spacer(modifier = Modifier.height(16.dp))
+            Spacer(modifier = Modifier.height(4.dp))
 
-            LazyColumn(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(horizontal = 8.dp)
-            ) {
-                items(favoriteTracks) { track ->
-                    TrackListItem(
-                        track = track,
-                        onClick = { onTrackClick(track) }
+            if (favoriteTracks.isEmpty()) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    Spacer(modifier = Modifier.height(150.dp))
+
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_music_not_found),
+                        contentDescription = "Music not found",
+                        modifier = Modifier
+                            .width(Dimensions.MusicNotFoundSize)
+                            .height(Dimensions.MusicNotFoundSize),
+                        alignment = Alignment.Center
                     )
+
+                    Spacer(modifier = Modifier.height(16.dp))
+
+                    Text(
+                        text = stringResource(R.string.favourites_media_empty_text),
+                        style = MaterialTheme.typography.bodyLarge.copy(
+                            color = MaterialTheme.colorScheme.onPrimary
+                        )
+                    )
+                }
+            }
+
+            else {
+                LazyColumn(
+                    modifier = Modifier
+                        .fillMaxSize()
+                ) {
+                    items(favoriteTracks) { track ->
+                        TrackListItem(
+                            track = track,
+                            onClick = { onTrackClick(track) }
+                        )
+                    }
                 }
             }
         }
